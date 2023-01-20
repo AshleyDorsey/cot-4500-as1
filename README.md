119fbd7e438294131be0d677c78b6a29c1eb2363
Ashley Dorsey <ashley.dorsey@knights.ucf.edu>
2023-01-20 11:53:07 -0500

    Start
    Just the outline of stuff. Nothing really started.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # cot-4500-as1
  
+#### By **Ashley Dorsey**
+
+#### Assignment covering topics from chapters 1 and 2.
+
+## Technologies Used
+
+- GitHub
+
+- Python
+
+## Description
+
+{I'm not really sure what this does, yet. I'm doing this before I do anything else...}
+
+## Setup/Installation Requirements
+
+{I don't know yet, but use bullet points.}
---
+++ src/main/assignment_1.py
@@ -0,0 +1,25 @@
+# Question 1: Use double precision, calculate the resulting values (format to 5 decimal places)
+#       a) 010000000111111010111001
+
+
+
+# Question 2: Repeat question 1 using three-digit chopping arithmetic
+
+
+
+# Question 3: Repeat question 1 using three-digit rounding arithmetic
+
+
+
+# Question 4: Compute the absolute and relative error with the exact value form question 1 and its 3 digit rounding
+
+
+
+# Question 5: What is the minimum number of terms needed to computer f(1) with error <10^-4?
+
+
+
+# Question 6: Determine the number of iterations necessary to solve f(x) = x^3 + 4x^2 - 10 = 0 with accuracy 10^-4 
+# using a = 4 and b = 7.
+#       b) Using the bisection method
+#       c) Using the newton Raphson method
